af1e030343c5cf69d414b3bab1863f3ca3c03cfa
ms161 <msehrawat161@gmail.com>
2024-09-08 16:40:34 +0530

    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:['https://almabetter-capstone-entertanment-app-api.vercel.app'],
+    origin:['https://almabetter-capstone-entertanment-app-s6z8.vercel.app'],
     credentials:true
 }))
 
